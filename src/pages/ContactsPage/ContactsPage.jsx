65146d18a389391fd529268fa4de1035c88def6a
Roman Hudz <hudzrom@gmail.com>
2024-12-16 17:35:50 +0200

    Done

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import ContactForm from '../../components/ContactForm/ContactForm';
 import ContactList from '../../components/ContactList/ContactList';
 import SearchBox from '../../components/SearchBox/SearchBox';
 import s from './ContactsPage.module.css';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from '../../redux/contacts/operations';
 
 const ContactsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={s.app_wrapper}>
       <div>
